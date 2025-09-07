388a442a9a473dcd30a462950e288a5996e108fd
savjaylade84 <savjaylade84@gmail.com>
2025-09-07 21:15:53 +0800

    added project card list  in portfolio

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask,render_template,request
-from dotenv import load_dotenv
+from flask import Flask,render_template,url_for
+import json
+import os
 
 app = Flask(__name__)
+information:dict = {}
 
-@app.route("/")
-def index() -> None:
-    pass
+def load_information() -> dict:
+    with open("information/information.json",'r') as file:
+        return json.load(file)
+    
+    return {}
+
+@app.route("/", methods = ['GET','POST'])
+def index():
+    information = load_information()
+    return render_template('index.html',information = information)
 
 
+if __name__ == '__main__':
+    app.run(debug=True, port = 8080)
