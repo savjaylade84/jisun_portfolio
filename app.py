2c25abf7e543088d6d7297de728fa89129ef7c6f
savjaylade84 <savjaylade84@gmail.com>
2025-09-29 23:35:40 +0800

    prototype of sending email but i comment the codes for now

--- app.py
+++ app.py
@@ -1,15 +1,52 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for,jsonify,request
+from flask_mail import Mail, Message
 import json
+from dotenv import load_dotenv
 import os
 
 app = Flask(__name__)
 information:dict = {}
+
+load_dotenv()
+
+# config for email sending
+app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
+app.config['MAIL_PORT'] = os.getenv('MAIL_PORT')
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS')
+app.config['MAIL_USE_SSL'] = os.getenv('MAIL_USE_SSL')
+app.config['MAIL_DEFAULT_SENDER'] = ('<your name sender>', '<your-email>@gmail.com')
+
+mail = Mail(app)
 
 def load_information() -> dict:
     with open("information/information.json",'r') as file:
         return json.load(file)
     
     return {}
+
+""" @app.route('/send_email', methods=['POST'])
+def send_email():
+    data = request.get_json()
+
+    subject = data.get('title')
+    recipient = data.get('email')   # single recipient
+    body = data.get('message')
+
+    if not subject or not recipient or not body:
+        return jsonify({"message": "The fields title, email, and message are required"}), 400
+
+    msg = Message(
+        subject=subject,
+        recipients=[recipient],  # still needs to be a list
+        body=body
+    )
+    mail.send(msg)
+
+    return jsonify({"message": "Email sent successfully"}), 200 """
+
+    
 
 @app.route("/", methods = ['GET','POST'])
 def index():
